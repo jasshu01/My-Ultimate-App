17c8c703ca3daa0e309584833334c63fc99d2720
jasshugarg <jashugarg266@gmail.com>
2022-11-02 16:52:39 +0530

    Splash Screen added

--- app/src/main/java/com/example/myultimateapp/MainActivity.java
+++ app/src/main/java/com/example/myultimateapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.myultimateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,
+                        HomePage.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
