7c0ae65694398c95cf8e212b770d6ea3fd00fb4e
jasshugarg <jashugarg266@gmail.com>
2022-11-05 07:41:26 +0530

    Navigation Layout setup Done

--- app/src/main/java/com/example/myultimateapp/MainPageActivity.java
+++ app/src/main/java/com/example/myultimateapp/MainPageActivity.java
@@ -1,12 +1,20 @@
 package com.example.myultimateapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -21,20 +29,65 @@ public class MainPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        drawerLayout=findViewById(R.id.drawerLayout);
-        navigationView=findViewById(R.id.navigationView);
-        toolbar=findViewById(R.id.toolbar);
-
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
 
 
         setSupportActionBar(toolbar);
 
-        @SuppressLint("ResourceType") ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,0,1);
+        @SuppressLint("ResourceType") ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openNavigation, R.string.closeNavigation);
 
         drawerLayout.addDrawerListener(toggle);
 
         toggle.syncState();
+        loadFragment(new JokesFragment(MainPageActivity.this),0);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id) {
+                    case R.id.navigationItemHomePage:
+                        loadFragment(new DogImagesFragment(MainPageActivity.this),1);
+                        break;
+                    case R.id.navigationItemLogOut:
+                        loadFragment(new ActivitiesFragment(MainPageActivity.this),1);
+                        break;
+                    case R.id.navigationItemJokesPage: {
+                        loadFragment(new JokesFragment(MainPageActivity.this),1);
+                        break;
+                    }
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+
+    }
+
+
+    private void loadFragment(Fragment fragment, int flag) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if (flag == 0)
+            ft.add(R.id.container, fragment);
+        else
+            ft.replace(R.id.container, fragment);
+        ft.commit();
     }
 }
