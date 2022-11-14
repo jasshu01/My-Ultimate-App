92f08a5ce43f7fb8838d5beae6a8f943ba303b7d
jasshugarg <jashugarg266@gmail.com>
2022-11-14 13:31:04 +0530

    service started by external app

---
+++ app/src/main/java/com/example/myultimateapp/MySenderService.java
@@ -0,0 +1,25 @@
+//package com.example.myultimateapp;
+//
+//import android.app.Service;
+//import android.content.Intent;
+//import android.os.IBinder;
+//
+//public class MySenderService extends Service {
+//    public MySenderService() {
+//    }
+//
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//    }
+//
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId) {
+//        return super.onStartCommand(intent, flags, startId);
+//    }
+//}
--- app/src/main/java/com/example/myultimateapp/SendBroadCastFragment.java
+++ app/src/main/java/com/example/myultimateapp/SendBroadCastFragment.java
@@ -1,17 +1,22 @@
 package com.example.myultimateapp;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.security.Provider;
 
 public class SendBroadCastFragment extends Fragment {
 
@@ -41,10 +46,36 @@ public class SendBroadCastFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String msg= String.valueOf(message.getText());
-                Intent intent=new Intent();
+                String msg = String.valueOf(message.getText());
+
+
+                Intent i = new Intent("com.example.mybroadcastreceiverapp.START_MY_SERVICE");
+                i.setPackage("com.example.mybroadcastreceiverapp");
+
+//                i.setComponent(new ComponentName("com.example.mybroadcastreceiverapp", "com.example.mybroadcastreceiverapp.service.MyIntentService"));
+
+                try {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        getContext().startForegroundService(i);
+                        Log.d("NEWSTACK", "starting service inultimate 1 " + i.getAction());
+
+                    }
+                    else
+                        getContext().startService(i);
+                    Log.d("NEWSTACK", "starting service inultimate 2 " + i.getAction());
+
+
+
+                } catch (Exception e) {
+                    Log.d("NEWSTACK", e.toString());
+                }
+
+
+
+                Intent intent = new Intent();
                 intent.setAction("com.jasshugarg.ultimateappsender");
-                intent.putExtra("Broadcast Message",msg);
+                intent.putExtra("Broadcast Message", msg);
                 getContext().sendBroadcast(intent);
                 Toast.makeText(getContext(), "Message Broadcasted", Toast.LENGTH_SHORT).show();
                 message.setText("");
