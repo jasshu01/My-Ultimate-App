afd170f317ad3ea8b7becd541c9a00c596143271
jasshugarg <jashugarg266@gmail.com>
2022-12-05 17:53:28 +0530

    showing user image in "Your Profile Fragment "

--- app/src/main/java/com/example/myultimateapp/YourProfileFragment.java
+++ app/src/main/java/com/example/myultimateapp/YourProfileFragment.java
@@ -4,6 +4,7 @@ import static android.content.Context.MODE_PRIVATE;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
 
@@ -20,12 +21,11 @@ import android.widget.TextView;
 public class YourProfileFragment extends Fragment {
 
     ImageView displayPic;
-    TextView name,username,email,phone,dob,address,postalcode;
+    TextView name, username, email, phone, dob, address, postalcode;
 
     public YourProfileFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -39,34 +39,40 @@ public class YourProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_your_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_your_profile, container, false);
 
-        displayPic=view.findViewById(R.id.yourProfile_ProfilePic);
-        name=view.findViewById(R.id.yourProfile_Name);
-        username=view.findViewById(R.id.yourProfile_UserName);
-        email=view.findViewById(R.id.yourProfile_Email);
-        phone=view.findViewById(R.id.yourProfile__phone);
-        dob=view.findViewById(R.id.yourProfile_dob);
-        address=view.findViewById(R.id.yourProfile_Address);
-        postalcode=view.findViewById(R.id.yourProfile_PostalCode);
+        displayPic = view.findViewById(R.id.yourProfile_ProfilePic);
+        name = view.findViewById(R.id.yourProfile_Name);
+        username = view.findViewById(R.id.yourProfile_UserName);
+        email = view.findViewById(R.id.yourProfile_Email);
+        phone = view.findViewById(R.id.yourProfile__phone);
+        dob = view.findViewById(R.id.yourProfile_dob);
+        address = view.findViewById(R.id.yourProfile_Address);
+        postalcode = view.findViewById(R.id.yourProfile_PostalCode);
 
-        dbHandler handler=new dbHandler(getContext(),"myApp",null,1);
+        dbHandler handler = new dbHandler(getContext(), "myApp", null, 1);
 
         SharedPreferences sp = getActivity().getSharedPreferences("Current User", MODE_PRIVATE);
-        String currentUser=sp.getString("LoggedInUser","");
+        String currentUser = sp.getString("LoggedInUser", "");
 
-        UserDetails user=handler.fetchUserUsingUserName(currentUser);
+        UserDetails user = handler.fetchUserUsingUserName(currentUser);
 
-//        jasshugarg - Jasshu@123
+
+        Bitmap myImage = handler.StringToBitMap(user.getImageurls());
+        if (myImage != null)
+            displayPic.setImageBitmap(myImage);
+        else
+            displayPic.setImageResource(R.drawable.person);
+
 
 //        Log.d("profilepage", "onCreateView: "+user.toString());
-        name.setText("Name: "+user.getTitle()+" "+user.getFirstName()+" "+user.getLastName());
-        username.setText("Username: "+user.getUsername());
-        email.setText("Email: "+user.getEmail());
-        phone.setText("Phone: "+user.getPhone());
-        dob.setText("DOB: "+user.getDob());
-        address.setText("Address: "+user.getAddress());
-        postalcode.setText("Postal Code: "+user.getPostalcode());
+        name.setText("Name: " + user.getTitle() + " " + user.getFirstName() + " " + user.getLastName());
+        username.setText("Username: " + user.getUsername());
+        email.setText("Email: " + user.getEmail());
+        phone.setText("Phone: " + user.getPhone());
+        dob.setText("DOB: " + user.getDob());
+        address.setText("Address: " + user.getAddress());
+        postalcode.setText("Postal Code: " + user.getPostalcode());
 
         return view;
     }
